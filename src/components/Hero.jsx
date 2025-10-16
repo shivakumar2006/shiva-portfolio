755b85ddafd039f653e74c33a8159f25009354fd
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-16 16:33:25 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import Particles from './ui/particles'
 import PillNav from './ui/Pillnav';
 import logo from "../assets/logo.jpeg";
+import SplitText from "./ui/SplitText";
+import ShinyText from './ui/ShinyText';
 
 const Hero = () => {
+
+    const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
+
   return (
     <div className='w-screen min-h-screen bg-black relative overflow-hidden'>
 
@@ -53,9 +60,36 @@ const Hero = () => {
     </div>
 
       {/* 🖤 Center Hero Content */}
-      <div className='absolute inset-0 flex flex-col items-center justify-center text-white z-10 pointer-events-none'>
-        <h1 className='text-5xl font-bold mb-4 pointer-events-auto'>Welcome to My Portfolio</h1>
-        <p className='text-lg text-gray-300 pointer-events-auto'>I build modern web experiences</p>
+      <div className='absolute inset-0 flex flex-row items-center justify-center text-white z-10 pointer-events-none'>
+        <div className='w-200 h-full flex flex-col justify-center items-center gap-10'>
+            <SplitText
+              text="Shiva Kumar"
+              className="text-8xl font-extrabold pr-40 text-center mt-30"
+              delay={100}
+              duration={0.6}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+
+            <ShinyText 
+              text=" Senior Software Engineer at @Victoria's Secret | ex @Verizon, @TechMahindra" 
+              disabled={false} 
+              speed={10} 
+              className='custom-class' 
+            />
+            <p className='text-xl px-10 font-extralight'>
+                Experienced Senior Software Engineer with 8.5+ years of expertise in Golang and Java, specializing in designing and developing scalable microservices, distributed systems, and cloud-native applications. Skilled in Golang, Java, Spring Boot, Microservices Architecture, Hibernate, JPA, SQL Databases, Redis, Kafka, Docker, Kubernetes, Git, Jenkins, Azure, AWS, Data Structures, Algorithms, and System Design. Experienced in implementing CI/CD pipelines, automation, and monitoring solutions. Strong engineering professional with a Bachelor of Technology (B.Tech) in Computer Science & Engineering, with proven success delivering enterprise-grade solutions in Agile environments.
+            </p>
+        </div>
+        <div className='w-160 h-full border-white border flex flex-col justify-center items-center'>
+              World
+        </div>
       </div>
 
     </div>
---
+++ src/components/ui/ShinyText.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div
+      className={`text-[#bbb9b9a4] pr-20 text-lg bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
+      style={{
+        backgroundImage:
+          'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
+        backgroundSize: '200% 100%',
+        WebkitBackgroundClip: 'text',
+        animationDuration: animationDuration
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ShinyText;
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         shine: {
+//           '0%': { 'background-position': '100%' },
+//           '100%': { 'background-position': '-100%' },
+//         },
+//       },
+//       animation: {
+//         shine: 'shine 5s linear infinite',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
